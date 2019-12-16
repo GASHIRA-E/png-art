e2aa1f7275957f03b4a72b029aafaa82f2a962a2
GASHIRA-E <dsteams@outlook.jp>
2019-12-17 01:25:58 +0900

    eventをとれた

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-const hoge = 'hoge';
+const textArea = document.getElementById('js-test');
 
-const fuga = `${hoge}_fuga`;
-
-console.log(fuga);
+window.addEventListener('deviceorientation', function(event) {
+  console.log(event);
+  textArea.value = `pitch:${event.beta}\nyaw:${event.gamma}\nrole:${event.alpha}`;
+});
+document.getElementById("doeSupported").innerText = "Supported!";

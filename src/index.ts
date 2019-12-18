7f40cd81de271d0893f6c335cf93bcbc83eb3646
GASHIRA <d.gashira@GASHIRAnoMacBook-Pro.local>
2019-12-19 08:16:55 +0900

    pixi.jsのhello world

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,16 @@
+import * as PIXI from 'pixi.js';
+
 const textArea: HTMLTextAreaElement | null = document.getElementById('js-test') as HTMLTextAreaElement;
 
 window.addEventListener('deviceorientation', function(event: DeviceOrientationEvent) {
   console.log(event);
   if (!textArea) return;
   textArea.value = `pitch:${event.beta}\nyaw:${event.gamma}\nrole:${event.alpha}`;
+});
+
+window.addEventListener('load', () => {
+  const app = new PIXI.Application({width: 256, height: 256});
+  const canvas = document.getElementById('myBoard') as HTMLCanvasElement;
+  if (!canvas) return;
+  canvas.appendChild(app.view);
 });
